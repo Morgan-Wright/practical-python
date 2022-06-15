07a1edde01bb063d09957f91f94411071f2d53fe
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-06-14 20:03:57 -0700

    strings, integers, and floats complete

---
+++ str_int_float.py
@@ -0,0 +1,10 @@
+name = "Morgan"
+print(name)
+
+store_name = "Morgan's store"
+print(store_name)
+
+name = input("Hello, what is your name?\n")
+hello = "Hello "
+greeting = hello + name
+print(greeting)
