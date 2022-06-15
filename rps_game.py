e1de463fc82307a9089d82d42fbdddebd6bd773d
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-06-14 21:02:54 -0700

    rps_game complete

--- rps_game.py
+++ rps_game.py
@@ -1,1 +1,16 @@
-computers_choice =
+import random
+
+computer_choice = random.choice(['scissors', 'rock', 'paper'])
+user_choice = input('Rock, Papers, Scissors, Shoot! \n')
+winner = ('win, the computer had ' + computer_choice)
+
+if computer_choice == user_choice:
+    print('tie')
+elif user_choice == 'rock' and computer_choice == 'scissors':
+    print(winner)
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print(winner)
+elif user_choice == 'scissors' and computer_choice == 'paper':
+    print(winner)
+else:
+    print('You lose')
