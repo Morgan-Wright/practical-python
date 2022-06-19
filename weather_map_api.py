36f23885310f23a185dc7777b8869577ca664274
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-06-19 16:35:33 -0700

    weather api updated with functions

--- weather_map_api.py
+++ weather_map_api.py
@@ -1,15 +1,24 @@
 import requests
 
-api_key = "c69ab9c9561b2cbf754c801a93c2929a"
-city = "Orlando"
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
 
-request = requests.get(url)
-json = request.json()
+def get_weather_temp():
+    api_key = "c69ab9c9561b2cbf754c801a93c2929a"
+    city = "Orlando"
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + \
+        city+"&appid="+api_key+"&units=imperial"
 
-description = json.get("weather")[0].get("description")
-print(description)
-min_temp = json.get("main").get("min_temp")
-print(min_temp)
-max_temp = json.get("main").get("max_temp")
-print(max_temp)
+    request = requests.get(url)
+    json = request.json()
+
+    description = json.get("weather")[0].get("description")
+    min_temp = json.get("main").get("min_temp")
+    max_temp = json.get("main").get("max_temp")
+    return {'description': description, 
+            'min_temp': min_temp,
+            'max_temp': max_temp
+            }
+
+weather_dict = get_weather_temp()
+print(weather_dict.get('description'))
+print(weather_dict.get('min_temp'))
+print(weather_dict.get('max_temp'))
